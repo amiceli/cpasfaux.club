37c5d8e8633f0f5f2e625fd2d686c8f8bcea0350
amiceli <miceli.antoine@gmail.com>
2015-02-17 12:57:10 +0100

    Ajout song burgonde et changement de l'icone sur le bouton play

--- public/js/jukeboxes.js
+++ public/js/jukeboxes.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
 	
 	$('.jukeboxes .view button').click(function() {
 		$(this).parents('.view').find('audio').trigger('play');
+		$(this).find('i').prop('class', 'fa fa-pause');
+
+
+		$(this).parents('.view').find('audio').bind('ended', $.proxy(function() {
+			$(this).find('i').prop('class', 'fa fa-play');
+		}, this));
 	});
 
 });
