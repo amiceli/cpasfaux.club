e95846ce4c3310b9505163645c6192bda5a422d8
amiceli <miceli.antoine@gmail.com>
2015-02-25 19:47:10 +0100

    add slider to all routes

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php'; 
+require_once __DIR__.'/vendor/autoload.php';
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
 
 $app['debug'] = true;
 
@@ -10,12 +10,38 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/', function() use($app) { 
-    return $app['twig']->render('pages/index.html.twig');
-}); 
+$app->get('/', function() use($app) {
 
-$app->get('/jukebox', function() use($app) { 
-    return $app['twig']->render('pages/jukebox.html.twig');
-}); 
+    $files = array_diff(scandir(__DIR__ . '/public/img/slider'), array('.', '..'));
+    $returnedFiled = [];
+
+    foreach ($files as $each) {
+        array_push($returnedFiled, [
+            "filename" => $each,
+            "alt" => preg_replace('/\\.[^.\\s]{3,4}$/', '', $each)
+        ]);
+    }
+
+    return $app['twig']->render('pages/index.html.twig', array(
+        "images" => $returnedFiled
+    ));
+});
+
+$app->get('/jukebox', function() use($app) {
+
+    $files = array_diff(scandir(__DIR__ . '/public/img/slider'), array('.', '..'));
+    $returnedFiled = [];
+
+    foreach ($files as $each) {
+        array_push($returnedFiled, [
+            "filename" => $each,
+            "alt" => preg_replace('/\\.[^.\\s]{3,4}$/', '', $each)
+        ]);
+    }
+    
+    return $app['twig']->render('pages/jukebox.html.twig', array(
+        "images" => $returnedFiled
+    ));
+});
 
 $app->run();
