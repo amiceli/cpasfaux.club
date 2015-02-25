f104555ede0112f65c86b0f25089a2d2b4a93e23
amiceli <miceli.antoine@gmail.com>
2015-02-25 19:47:02 +0100

    add image slider

---
+++ public/js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    /*var interval = null, offset = 1;
+
+    interval = setInterval(function() {
+        if (offset === 4) {
+            $('header .slider').animate({
+                marginLeft : 0
+            }, 500);
+            clearInterval(interval);
+        } else {
+            $('header .slider').animate({
+                marginLeft : (-offset*100) + '%'
+            }, 500, 'linear');
+            offset++;
+        }
+    }, 5000);*/
+});
