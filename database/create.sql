5a42f2be6d98c1d70e116c3f95ef46a33c2bd968
amiceli <miceli.antoine@gmail.com>
2015-04-06 19:00:22 +0200

    update import

--- database/create.sql
+++ database/create.sql
@@ -1,22 +1,8 @@
 SET NAMES utf8;
 SET time_zone = '+00:00';
 
-CREATE DATABASE `kaamelot` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `kaamelot`;
-
-CREATE TABLE `citation` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `episode` varchar(255) NOT NULL,
-  `idPersonnage` int(11) NOT NULL,
-  `idLivre` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idLivre` (`idLivre`),
-  KEY `idPersonnage` (`idPersonnage`),
-  CONSTRAINT `citation_ibfk_2` FOREIGN KEY (`idPersonnage`) REFERENCES `personnage` (`id`),
-  CONSTRAINT `citation_ibfk_1` FOREIGN KEY (`idLivre`) REFERENCES `livre` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+CREATE DATABASE `cpasfaux` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cpasfaux`;
 
 CREATE TABLE `livre` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,4 +17,17 @@ CREATE TABLE `personnage` (
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `citation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `episode` varchar(255) NOT NULL,
+  `idPersonnage` int(11) NOT NULL,
+  `idLivre` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idLivre` (`idLivre`),
+  KEY `idPersonnage` (`idPersonnage`),
+  CONSTRAINT `citation_ibfk_2` FOREIGN KEY (`idPersonnage`) REFERENCES `personnage` (`id`),
+  CONSTRAINT `citation_ibfk_1` FOREIGN KEY (`idLivre`) REFERENCES `livre` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- imports/index.php
+++ imports/index.php
@@ -1,15 +1,20 @@
 <?php
 
+    function display($var) {
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+    }
 	//	------------------------------------------------------------------
 	//	DATABSE CONFIGURATION
 	//	------------------------------------------------------------------
-	$databaseName     = 'kaamelott';
+	$databaseName     = 'cpasfaux';
 	$databaseUser     = 'root';
 	$databasePassword = 'toni';
 	$host             = 'localhost';
 
 	//	Database connexion
-	$database = new PDO("mysql:host=$host;dbname=$databaseName", $databaseUser, $databasePassword);
+	$database = new PDO("mysql:host=$host;dbname=$databaseName;charset=utf8", $databaseUser, $databasePassword);
 
 	//	------------------------------------------------------------------
 	//	REQUETE PREPARÉE	
@@ -18,30 +23,41 @@
 	$addLivreSQL      = $database->prepare('INSERT INTO livre (nom) VALUES (?);');
 	$addCitationSQL   = $database->prepare('INSERT INTO citation (content, episode, idPersonnage, idLivre) VALUES (?, ?, ?, ?);');
 
+    $getUserIDSQL = $database->prepare('select id from personnage where nom = ?;');
+    $getLivreIDSQL = $database->prepare('select id from livre where nom = ?;');
+
 	//	------------------------------------------------------------------
 	//	RECUPERATION DU CONTENU JSON
 	//	------------------------------------------------------------------
 	$content = file_get_contents('data.json');
 	$json    = json_decode($content);
 
+    $i = 0;
 	
 	foreach ($json as $index => $element) {
 
-		//	Ajout des persos
 		$addPersonnageSQL->execute(array( $index ));
-
-		$lastUserId = $database->lastInsertId();
+        $getUserIDSQL->execute(array($index));
+        $id = $getUserIDSQL->fetch();
+        $lastUserId = $id[0];
 
 		//	Ajout des livres
 		foreach ($json->$index as $idx => $el) {
 
 			$addLivreSQL->execute(array( $idx ));
-
-			$lastLivreId = $database->lastInsertId();
+            $getLivreIDSQL->execute(array( $idx ));
+            $id2 = $getLivreIDSQL->fetch();
+            echo 'id2';
+            display($id2);
+            $lastLivreId = $id2[0];
 
 			foreach ($json->$index->$idx as $key => $value) {
-				//	Citation, enfin
-				$addCitationSQL->execute(array( $value[0], $value[1], $lastUserId, $lastLivreId ));
+				try {
+                    display(array( $value[0], $value[1], $lastUserId, $lastLivreId ));
+                    $addCitationSQL->execute(array( $value[0], $value[1], $lastUserId, $lastLivreId ));
+                } catch(\Exception $e) {
+                    display($e);
+                }
 			}
 		}
 	}
